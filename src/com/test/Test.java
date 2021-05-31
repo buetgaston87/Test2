c1c74decd436d64082c631e02d839a30815f181b
Admin <Admin@Gaby>
2021-05-31 19:42:55 +0200

    mergeado

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gasti capo");
+		String name = "Gaby";
+		System.out.println(name);
+		String value = "valor de la variables";
+		int numero = 2;
+		System.err.println(numero + value);
 	}
 
 }
