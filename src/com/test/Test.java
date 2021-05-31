3a42f4c4531867a211fa13be41fe12ba60d9932a
buetgaston87 <buetgaston87@gmail.com>
2021-05-31 19:39:34 +0200

    Variable name

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gasti capo");
+		String name = "Gaby";
+		System.out.println(name);
 	}
 
 }
